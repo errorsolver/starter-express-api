684a91f7f6ce3745610fac969e712116cc8c9215
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2023-05-22 21:29:39 +0700

    change minor

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const routes = require('./src/routes/authRoutes')
 const app = express()
 app.use(cors())
 app.use(express.json())
-
-db.connect()
-
 app.use(routes)
-app.listen(process.env.PORT || 3000, (req, res) => {
-    console.log(`localhost: ${process.env.PORT || 3000}`);
-})
+
+
+const port = process.env.PORT
+db.connect()
+    .then(() => app.listen(port || 3000), console.log(`Connected, Port: ${port}`))
+    .catch(err => console.log(err))
